8286b23092b5bd6d64448ea8f2f0bce3e2e296c7
ALamgir Hossain <alamgir.cse14.just@gmail.com>
2019-08-18 19:18:30 +0600

    input char array

---
+++ input char array using scanf in cpp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch[100];
+    cout<<"Enter the string: "<<endl;
+    scanf("%[^\n]%*c", ch);
+    cout<<endl<<"Entered string is: "<<ch<<endl;
+    return 0;
+}
